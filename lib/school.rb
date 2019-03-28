3f55edea086ceaf280ef9c13e61449fc6b53c2a8
David Stewart <davidstewart@gmail.com>
2019-03-28 02:11:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,16 @@
+
+require 'pry'
 class School 
+  
+  def add_student(student,grade)
+    @roster[grade] = student
+  end
   
   def initialize(name)
   @name = name
+  @roster = {}
   end
   
-   
+   binding.pry
   
 end
